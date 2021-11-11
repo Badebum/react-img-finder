9dd625dda8f61dd7a84aa9804575197ca02ba97e
Andrey <82231196+Badebum@users.noreply.github.com>
2021-11-11 16:56:45 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,29 @@
 import React, { Component } from 'react';
-import Section from './components/Section';
-import FeedbackOptions from './components/FeedbackOptions';
-import Notification from './components/Notification';
-import Statistics from './components/Statistics';
-import style from './App.module.css';
+import Button from './components/Button';
+import ImageGallery from './components/ImageGallery';
+import ImageGalleryItem from './components/ImageGalleryItem';
+import Loader from './components/Loader';
+import Modal from './components/Modal';
+import Searchbar from './components/Searchbar';
+
+
 
 class App extends Component {
   state = {
-    good: this.props.initialGood,
-    neutral: this.props.initialNeutral,
-    bad: this.props.initialBad,
+    
   };
-  static defaultProps = {
-    initialGood: 0,
-    initialNeutral: 0,
-    initialBad: 0,
-  };
-
-  handleChenge = options => {
-    const name = options.name;
-    this.setState(prevState => ({
-      [name]: prevState[name] + 1,
-    }));
-  };
-
-  countTotalFeedback() {
-    const { good, neutral, bad } = this.state;
-    const total = good + neutral + bad;
-    return total;
-  }
-
-  countPositiveFeedbackPercentage() {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
-    const positiveFeedbackPercentage = (good * 100) / total;
-    return Math.round(positiveFeedbackPercentage);
-  }
-
+  
   render() {
-    const { good, neutral, bad } = this.state;
-    const total = this.countTotalFeedback();
-    const percentage = this.countPositiveFeedbackPercentage();
+   
 
     return (
-      <div className={style.feedBack}>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
-            onLeaveFeedback={this.handleChenge}
-          />
-        </Section>
-
-        <Section title="Statistics">
-          {total === 0 ? (
-            <Notification message="No feedback given"></Notification>
-          ) : (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={total}
-              percentage={percentage}
-            />
-          )}
-        </Section>
+      <div >
+       <Button />
+       <ImageGallery />
+       <ImageGalleryItem />
+       <Loader />
+       <Modal />
+       <Searchbar />
       </div>
     );
   }
