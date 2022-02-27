75c08a726feb882f03c1ec0e2118283e0e045715
Andrey <82231196+Badebum@users.noreply.github.com>
2022-02-27 14:15:58 +0300

    new commit

--- src/components/API/Api.jsx
+++ src/components/API/Api.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseURL = 'https:pixabay.com/api/';
 const apiKey = '24292028-9ebf5f04d4d99405c3dd4ed49';
 
-const fetchHits = ({ currentPage = 1, searchQuery = '', pageSize = 3 }) => {
+const fetchHits = ({ currentPage = 1, searchQuery = '', pageSize = 9 }) => {
   return axios
     .get(
       `${baseURL}?q=${searchQuery}&page=${currentPage}&key=${apiKey}&image_type=photo&orientation=horizontal&per_page=${pageSize}`,
